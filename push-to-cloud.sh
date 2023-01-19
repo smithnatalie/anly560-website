c797a31edcfba68363d18b884f3c8f4cbe8d2a1e
Natalie Smith <nls73@georgetown.edu>
2023-01-19 01:33:09 -0500

    shell script fix

--- push-to-cloud.sh
+++ push-to-cloud.sh
@@ -4,9 +4,9 @@
 rsync -alvr _site/ smithnat@gtown.reclaimhosting.com:/home/smithnat/public_html/anly560
 
 # PUSH GIT REPO TO THE CLOUD FOR BACKUP
-DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
-message="GITHUB-UPLOAD:$DATE";
-echo "commit message = "$message; 
-git add ./; 
-git commit -m $message; 
-git push
+# DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
+# message="GITHUB-UPLOAD:$DATE";
+# echo "commit message = "$message; 
+# git add ./; 
+# git commit -m $message; 
+# git push
