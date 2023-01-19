0f948acd2ed64ef6cf8bda983357ce77295fc3df
Natalie Smith <nls73@georgetown.edu>
2023-01-18 20:38:06 -0500

    GITHUB-UPLOAD:DATE-2023-01-18-TIME-20-38-05

---
+++ push-to-cloud.sh
@@ -0,0 +1,12 @@
+#adding comment to push
+
+# SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER
+rsync -alvr --delete _site/ smithnat@gtown.reclaimhosting.com:/home/smithnat/public_html/anly560
+
+# PUSH GIT REPO TO THE CLOUD FOR BACKUP
+DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
+message="GITHUB-UPLOAD:$DATE";
+echo "commit message = "$message; 
+git add ./; 
+git commit -m $message; 
+git push
